9118fba82e9517613294fd9841e3202b876ee822
Jong Shin <jshin.m@gmail.com>
2022-01-12 04:43:43 -0500

    update list

--- Jovo/polytope-nsf-ai/util/__init__.py
+++ Jovo/polytope-nsf-ai/util/__init__.py
@@ -1,3 +1,3 @@
 # from .plots import *
 from .util import *
-from .polytope import generate_polytope, calculate_risks
+from .polytope import generate_polytope, calculate_risks, voronoi_finite_polygons_2d
