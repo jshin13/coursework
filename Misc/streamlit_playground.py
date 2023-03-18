f8c8a3d6566635ff34a15cd0b6c0388ead81f341
Jong Shin <jshin.m@gmail.com>
2023-03-18 00:57:24 -0400

    add progressive text generator

--- Misc/streamlit_playground.py
+++ Misc/streamlit_playground.py
@@ -6,7 +6,7 @@ import time
 st.title('test project')
 
 st_text = st.text('hello world')
-time.sleep(2)
+time.sleep(1)
 st_text.text('good bye')
 
 st.subheader('subheading')
@@ -16,5 +16,23 @@ values = np.random.rand(1,10)
 st.bar_chart(values)
 
 st.subheader('buttons')
-df = pd.read_excel('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
 st.write(df)
+
+st.spinner(text='In progress...') #Temporarily displays a message while executing a block of code
+
+text_demo = """It was the best of times, it was the worst of times, it was
+    the age of wisdom, it was the age of foolishness, it was
+    the epoch of belief, it was the epoch of incredulity, it
+    was the season of Light, it was the season of Darkness, it
+    was the spring of hope, it was the winter of despair, (...)
+    """
+
+field = ''
+txt = st.text_area('text_field', value=field)
+
+st.subheader('progressive texts')
+for i in text_demo.split(' '):
+    time.sleep(1)
+    field += i + ' '
+    st.text(field)
