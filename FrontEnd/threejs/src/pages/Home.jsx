4d5e8c110ac225a8d9fdc065928f86ffb15be951
Jong Shin <jshin.m@gmail.com>
2023-04-29 22:53:22 -0400

    test motion with AnimatePresence

--- FrontEnd/threejs/src/pages/Home.jsx
+++ FrontEnd/threejs/src/pages/Home.jsx
@@ -11,11 +11,48 @@ import {
   slideAnimation
 } from '../config/motion';
 
+import state from '../store';
+
 const Home = () => {
+  const snap = useSnapshot(state);
+
   return (
-    <div>
-      Home
-    </div>
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className='home' {...slideAnimation('left')}>
+          <motion.header {...slideAnimation('up')}>
+            <div class='text-xs'>
+              Header 1 - Start
+              <img 
+                src='./vite.svg'
+                alt='logo'
+                className='w-8 h-8 object-contain'
+              />
+              Header 1 - End
+            </div>
+          </motion.header>
+
+        <br />
+
+          <motion.header {...slideAnimation('down')}>
+            <div class='text-xs'>
+              Header 2 - Start
+              <img 
+                src='./vite.svg'
+                alt='logo'
+                className='w-8 h-8 object-contain'
+              />
+              Header 2 - End
+            </div>
+          </motion.header>
+
+
+
+          <br />
+
+        </motion.section>
+      )}
+    </AnimatePresence>
   )
 }
 
